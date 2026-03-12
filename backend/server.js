502031bb856ff877f45955d063bda2aa22b2f302
madhan-231105 <mathangopi23@gmail.com>
2026-03-12 10:22:35 +0530

    updated server to deplo

--- backend/server.js
+++ backend/server.js
@@ -130,6 +130,8 @@ app.get("/pipeline-status", async (req, res) => {
 /* -----------------------------
    Start Server
 --------------------------------*/
-app.listen(5000, () => {
-  console.log("🚀 Backend running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Backend running on port ${PORT}`);
 });
